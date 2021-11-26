84de6496f7d5d1d99da70e01afccfc0c04e0cd8b
ChenkaiMao97 <chenkai@mit.edu>
2021-11-26 14:57:05 -0800

    starting code after update

--- SinGAN/models.py
+++ SinGAN/models.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
-import numpy as np
-import torch.nn.functional as F
+
 
 
 class ConvBlock(nn.Sequential):
@@ -18,6 +17,41 @@ def weights_init(m):
     elif classname.find('Norm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc1   = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2   = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
    
 class WDiscriminator(nn.Module):
     def __init__(self, opt):
@@ -25,6 +59,8 @@ class WDiscriminator(nn.Module):
         self.is_cuda = torch.cuda.is_available()
         N = int(opt.nfc)
         self.head = ConvBlock(opt.nc_im,N,opt.ker_size,opt.padd_size,1)
+        self.channel=ChannelAttention(max(N,opt.min_nfc))
+        # self.spatial=SpatialAttention()
         self.body = nn.Sequential()
         for i in range(opt.num_layer-2):
             N = int(opt.nfc/pow(2,(i+1)))
@@ -34,6 +70,7 @@ class WDiscriminator(nn.Module):
 
     def forward(self,x):
         x = self.head(x)
+        x=self.channel(x)*x
         x = self.body(x)
         x = self.tail(x)
         return x
@@ -44,6 +81,9 @@ class GeneratorConcatSkip2CleanAdd(nn.Module):
         super(GeneratorConcatSkip2CleanAdd, self).__init__()
         self.is_cuda = torch.cuda.is_available()
         N = opt.nfc
+
+        self.channel = ChannelAttention(max(N, opt.min_nfc))
+        self.spatial = SpatialAttention()
         self.head = ConvBlock(opt.nc_im,N,opt.ker_size,opt.padd_size,1) #GenConvTransBlock(opt.nc_z,N,opt.ker_size,opt.padd_size,opt.stride)
         self.body = nn.Sequential()
         for i in range(opt.num_layer-2):
@@ -57,6 +97,8 @@ class GeneratorConcatSkip2CleanAdd(nn.Module):
     def forward(self,x,y):
         x = self.head(x)
         x = self.body(x)
+        x = self.channel(x) * x
+        x = self.spatial(x) * x
         x = self.tail(x)
         ind = int((y.shape[2]-x.shape[2])/2)
         y = y[:,:,ind:(y.shape[2]-ind),ind:(y.shape[3]-ind)]
